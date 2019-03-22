30e6898b8681ffc98892a8a26904f3b83c59f067
Konka Yamini <yamini.konka@gmail.com>
2019-03-22 19:51:52 +0530

    please check today exercises

--- Expand.java
+++ Expand.java
@@ -13,7 +13,27 @@ public class Expand {
 		//Hint: first start with inputs where there is always a number and then a character, then think about how
 		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
 		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
-		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
+		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee 
+		String string=scan.nextLine();
+            String store="0";
+		for(int i=0;i<string.length();i++){
+			char c=string.charAt(i);
+                if(Character.isDigit(c)){
+                   store+=c;
+                }else{
+
+                	int n=Integer.parseInt(store);
+                	if(n==0){
+                		System.out.print(c);
+                	}else{
+                		for(int j=0;j<n;j++){
+                		System.out.print(c);
+                	    }
+                	}
+                	
+                	store="0";
+                }
+		} 
 		
 	}
 	
--- Palindrome.java
+++ Palindrome.java
@@ -4,5 +4,47 @@ public class Palindrome{
 	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
 	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
 	//Ignore also spaces. 'Taco cat' is a valid palindrome.
+	public static boolean isPalindrome(String string){
+		String name1=string.toLowerCase();
+		//System.out.println(name);
+		// int index=name.length();
+		String name="";
+		//String store="";
+		for(int j=0;j<name1.length();j++){
+               // int c=0;
+			if(!Character.isWhitespace(name1.charAt(j))){
+                 name+=name1.charAt(j);
+                 //store="";
+			}//else{
+			//	store+=name1.charAt(j);
+			//}
+		}	
+			int index=name.length();
+		    for(int i=0;i<(int)Math.round(name.length()/2);i++){
+
+                if(name.charAt(i)==(name.charAt(index-1))){
+		    	// if(name.substring(i-1,i).equalsIgnoreCase(((name.substring(index-2,index-1))))){
+                    index--;
+                      if(i==index||i>index){
+                      	 return true;
+                      }
+                } else{
+                	return false;
+                }  
+		    }return true;
+		// }   
+	}
+    public static void main(String[] args){
+		// String exname="Nebel bei Siebleben ";
+		String exname="Taco cat ";
+		if(isPalindrome(exname)==true){
+			System.out.println(exname+" is palindrome");
+		}else{
+			System.out.println(exname+" is not a palindrome");
+		}
+
+		// String example = "Hannah";
+		// System.out.println( "substr test : " + example.toLowerCase() + example.length());
+	}
 
 }
